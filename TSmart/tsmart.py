d58fd7bf15199f8441a3ed51de66a3eb53267d3a
serialbandicoot <samtreweek@gmail.com>
2023-09-18 17:31:33 +0100

    Update tsmart.py

--- TSmart/tsmart.py
+++ TSmart/tsmart.py
@@ -4,14 +4,14 @@ from time import sleep
 import pathlib
 path = os.getcwd()
 os.chdir(os.getcwd()+"/TSmart")
-logging.critical("CWD: "+os.getcwd())
+logging.info("CWD: "+os.getcwd())
 #sys.path.append(str(THISPATH)+"\\TSmart")
 from settings import TSmartSettings
 
 logging.critical("Starting the Server...")
 serverpid=subprocess.Popen(["python3","server.py"])
 
-logging.critical("Starting Control Read loop")
+logging.info("Starting Control Read loop")
 
 while True:
     read.controlRead()
